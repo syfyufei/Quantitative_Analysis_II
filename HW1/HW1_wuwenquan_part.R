8a7ea9646e577a42c7a90bed4a68d5d19e0df51e
GohUnTsuan <39488085+GohUnTsuan@users.noreply.github.com>
2021-02-26 14:40:29 +0800

    Update HW1_wuwenquan_part.R
    p14, p16

--- HW1/HW1_wuwenquan_part.R
+++ HW1/HW1_wuwenquan_part.R
@@ -1,3 +1,11 @@
+##############################################
+#### Quantitative Analysis (II) (70700263)
+#### HW1
+#### Wu Wenquan 
+#### 2020312259
+##############################################
+
+
 ####load####
 library(foreign)
 library(stargazer)
@@ -23,3 +31,23 @@ plot_p10 <- ggplot(data, aes(as.character(data$mom_hs), kid_score)) +
   scale_x_discrete(labels = c('1' = "Yes",'0' = "No"))
 plot_p10
 
+# p14
+reg_p14 <- lm(kid_score ~ mom_iq + mom_hs )
+summary(reg_p14)
+# stargazer(reg_p14, type = 'text')
+
+# p16
+plot_p16 <- ggplot(data, aes(mom_iq, kid_score, 
+                             color = as.character(data$mom_hs),
+                             shape = as.character(data$mom_hs))) +
+  geom_hline(aes(yintercept = 50), color = 'grey90' ) +
+  geom_hline(aes(yintercept = 100), color = 'grey90' ) +
+  geom_point() + 
+  geom_smooth(method = lm, se = FALSE) +
+  theme_cowplot() +
+  scale_color_manual(values = c("black" ,"red")) +
+  scale_shape_manual(values = c(1, 19)) +
+  ylab('Child test scores') +
+  xlab('Mother IQ scores') +
+  theme(legend.position = 'none')
+plot_p16
