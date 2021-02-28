34d4476c6488a1644e0dc87bdd562596ccd5570f
GohUnTsuan <39488085+GohUnTsuan@users.noreply.github.com>
2021-03-01 02:24:37 +0800

    update p10 p16

--- HW0/HW1_wuwenquan_part.R
+++ HW0/HW1_wuwenquan_part.R
@@ -11,18 +11,22 @@ library(foreign)
 library(stargazer)
 library(tidyverse)
 library(cowplot)
+library(lm.beta)
 data <- read.dta('./kidiq.dta')
 attach(data)
 
 # p8
 reg_p8 <- lm(kid_score ~ mom_hs)
-summary(reg_p8)
-# stargazer(reg_p8, type = 'text')
+stargazer(reg_p8, type = 'text')
 
 # p10
 plot_p10 <- ggplot(data, aes(as.character(data$mom_hs), kid_score)) +
   geom_hline(aes(yintercept=50), color = 'grey90' ) +
   geom_hline(aes(yintercept=100), color = 'grey90' ) +
+  # geom_line(aes(data$mom_hs, kid_score)) +
+  stat_smooth(data, aes(as.numeric(data$mom_hs), kid_score, group = NULL)),
+method = 'glm', se = FALSE,) +
+  geom_line(position="identity") +
   geom_point(position = position_jitter(0.05), shape = 1, size = 2) +
   theme_cowplot() +
   ylab('Child test scores') +
@@ -32,21 +36,38 @@ plot_p10 <- ggplot(data, aes(as.character(data$mom_hs), kid_score)) +
   geom_smooth(method = lm, se = FALSE)
 plot_p10
 
+
+ggplot(data, aes(as.character(data$mom_hs), kid_score)) +
+  geom_line()
+()
+# p11
+reg_p11 <- lm(kid_score ~ mom_iq, data = data)
+stargazer(reg_p11, type = 'text')
+
+# p13
+plot_p13 <- ggplot(data, aes(x = mom_iq,y = kid_score)) + 
+  theme_cowplot() +
+  geom_hline(aes(yintercept=50), color = 'grey90' ) +
+  geom_hline(aes(yintercept=100), color = 'grey90' ) +
+  geom_point(shape=19, color = '#660874') +
+  xlab("Mother IQ score") + 
+  ylab("Child test scores") + 
+  geom_smooth(method = lm, se = FALSE, color = 'red')
+plot_p13
+
 # p14
 reg_p14 <- lm(kid_score ~ mom_iq + mom_hs )
-summary(reg_p14)
-# stargazer(reg_p14, type = 'text')
+stargazer(reg_p14, type = 'text')
 
 # p16
+reg_p14
 plot_p16 <- ggplot(data, aes(mom_iq, kid_score, 
                              color = as.character(data$mom_hs),
                              shape = as.character(data$mom_hs))) +
   geom_hline(aes(yintercept = 50), color = 'grey90' ) +
   geom_hline(aes(yintercept = 100), color = 'grey90' ) +
   geom_point() + 
-  geom_smooth(aes(mom_iq, kid_score, color = as.character(data$mom_hs)),
-              formula = y ~ x,
-              method = lm, se = FALSE) +
+  geom_smooth(aes(mom_iq, fitted(reg_p14))) +
   theme_cowplot() +
   scale_color_manual(values = c("black" ,"red")) +
   scale_shape_manual(values = c(1, 19)) +
@@ -55,7 +76,27 @@ plot_p16 <- ggplot(data, aes(mom_iq, kid_score,
   theme(legend.position = 'none')
 plot_p16
 
-# p25
+# p20
+reg_p20 <- lm(kid_score ~ mom_iq + mom_hs + mom_iq*mom_hs, data = data)
+stargazer(reg_p20, type = 'text')
+
+# p22
+plot_p22 <- ggplot(data, aes(mom_iq, kid_score, 
+                             color = as.character(mom_hs),
+                             shape = as.character(mom_hs))) +
+  geom_hline(aes(yintercept = 50), color = 'grey90' ) +
+  geom_hline(aes(yintercept = 100), color = 'grey90' ) +
+  geom_point() + 
+  theme_cowplot() +
+  scale_color_manual(values = c("black" ,"red")) +
+  scale_shape_manual(values = c(1, 19)) +
+  ylab('Child test scores') +
+  xlab('Mother IQ scores') +
+  theme(legend.position = 'none') +
+  geom_smooth(aes(mom_iq, kid_score), method = lm, formula = y ~ x, se = FALSE)
+plot_p22
+
+# p25, but I think there may have some wrong in page 25, because it calculate  sd(x) but didn't use sd(x) in next step. 
 normalize <- function(x){
   norm_x <- (x - mean(x))
   return(norm_x)
@@ -66,16 +107,7 @@ data$stdmom_iq <- normalize(mom_iq)
 data$stdmom_hs <- normalize(mom_hs)
 
 reg_p25 <- lm(kid_score ~ stdmom_iq + stdmom_hs + I(stdmom_iq*stdmom_hs), data = data)
-summary(reg_p25)
-# stargazer(reg_p25, type = 'text')
-
-
-
-data$cmom_iq <- mom_iq - mean(mom_iq)
-data$cmom_hs <- mom_hs - mean(mom_hs)
-
-reg_p25 <- lm(kid_score ~ cmom_iq + cmom_hs + I(cmom_iq*cmom_hs))
-summary(reg_p25)
+stargazer(reg_p25, type = 'text')
 
 # p27
 normalize2 <- function(x){
@@ -88,15 +120,22 @@ data$std2mom_hs <- normalize2(mom_hs)
 
 
 reg_p27 <- lm(kid_score ~ std2mom_iq + std2mom_hs + I(std2mom_iq*std2mom_hs), data = data)
-summary(reg_p27)
-# stargazer(reg_p27, type = 'text')
+stargazer(reg_p27, type = 'text')
+
+# p30
+reg_p30 <- lm(kid_score ~ mom_iq + mom_hs, data = data)
+stargazer(reg_p30, type = 'text')
+reg_p30_beta <- lm.beta(reg_p30)
+reg_p30_beta
+
+# p32
+reg_p32 <- lm(log(kid_score) ~ mom_iq + mom_hs, data = data)
+stargazer(reg_p32, type = 'text')
 
 # p33
 reg_p33 <- lm(kid_score ~ log(mom_iq) + mom_hs)
-summary(reg_p33)
-# stargazer(reg_p33, type = 'text')
+stargazer(reg_p33, type = 'text')
 
 # p39
 reg_p39 <- lm(kid_score ~ as.character(mom_work))
-summary(reg_p39)
-# stargazer(reg_p39, type = 'text')
+stargazer(reg_p39, type = 'text')
